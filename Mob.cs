f7ca53c93c0c33c681d28ea10f17c87abf5cba12
shiva <literallyshiva@gmail.com>
2026-09-01 02:59:07 +0300

    Добавил противников и их случайную генерацию

---
+++ Mob.cs
@@ -0,0 +1,30 @@
+using Godot;
+
+public partial class Mob : CharacterBody3D
+{
+    [Export]
+    public int MinSpeed { get; set; } = 10;
+
+    [Export]
+    public int MaxSpeed { get; set; } = 18;
+
+    public void Initialize(Vector3 startPosition, Vector3 playerPosition)
+    {
+        LookAtFromPosition(startPosition, playerPosition, Vector3.Up);
+        RotateY((float)GD.RandRange(-Mathf.Pi / 4.0, Mathf.Pi / 4.0));
+
+        int randomSpeed = GD.RandRange(MinSpeed, MaxSpeed);
+        Velocity = Vector3.Forward * randomSpeed;
+        Velocity = Velocity.Rotated(Vector3.Up, Rotation.Y);
+    }
+
+    public override void _PhysicsProcess(double delta)
+    {
+        MoveAndSlide();
+    }
+
+    private void OnVisibilityNotifierScreenExited()
+    {
+        QueueFree();
+    }
+}
